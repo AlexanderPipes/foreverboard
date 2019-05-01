8fd56c880bb8a1651854d6645a3f643f1c970359
apipes <alexpipes@outlook.com>
2019-04-30 21:46:50 -0400

    more project work

--- socket-client/src/index.js
+++ socket-client/src/index.js
@@ -4,5 +4,6 @@ import './index.css';
 import ForeverBoard from './ForeverBoard';
 import * as serviceWorker from './serviceWorker';
 
+// render my foreverboard at the root element (React stuff)
 ReactDOM.render(<ForeverBoard />, document.getElementById('root'));
 serviceWorker.unregister();
